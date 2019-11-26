400bdbc9e994dcda1d12af11d9436a3f39ba0f73
Dost Arora <31973060+dostarora97@users.noreply.github.com>
2019-11-27 02:50:21 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#Install Python3.7
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.7
+
+#Install pip
+sudo apt-get install python3-pip
+
+#Install Jupyter Notebook
+python3.7 -m pip install jupyter notebook
+
+#Install all requirements
+wget raw.githubusercontent.com/dostarora97/dse/master/ubuntu_py37_req.txt
+python3.7 -m pip install -r ubuntu_py37_req
+rm ubuntu_py37_req
+
+#Get all Ipython Notebooks
+wget raw.githubusercontent.com/dostarora97/dse/master/1.ipynb
+wget raw.githubusercontent.com/dostarora97/dse/master/2.ipynb
+wget raw.githubusercontent.com/dostarora97/dse/master/3.ipynb
+wget raw.githubusercontent.com/dostarora97/dse/master/4.ipynb
+wget raw.githubusercontent.com/dostarora97/dse/master/5.ipynb
+wget raw.githubusercontent.com/dostarora97/dse/master/6.ipynb
+wget raw.githubusercontent.com/dostarora97/dse/master/7.ipynb
+wget raw.githubusercontent.com/dostarora97/dse/master/8.ipynb
+wget raw.githubusercontent.com/dostarora97/dse/master/9.ipynb
+
+#Get all Datasets
+wget raw.githubusercontent.com/dostarora97/datasets/master/1.zip
+wget raw.githubusercontent.com/dostarora97/datasets/master/2.csv
+wget raw.githubusercontent.com/dostarora97/datasets/master/3.csv
+wget raw.githubusercontent.com/dostarora97/datasets/master/4.csv
+wget raw.githubusercontent.com/dostarora97/datasets/master/5.csv
+wget raw.githubusercontent.com/dostarora97/datasets/master/6.csv
+wget raw.githubusercontent.com/dostarora97/datasets/master/7.csv
+wget raw.githubusercontent.com/dostarora97/datasets/master/8.csv
+sudo apt install unzip
+unzip 1.zip
+rm 1.zip
